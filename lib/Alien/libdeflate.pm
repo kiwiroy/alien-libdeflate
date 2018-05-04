3081d6020941d0fec2a15ef73771e6eaba2b65f3
Roy Storey <kiwiroy@users.noreply.github.com>
2018-05-05 08:24:05 +1200

    add programs, pod and test

--- lib/Alien/libdeflate.pm
+++ lib/Alien/libdeflate.pm
@@ -9,3 +9,49 @@ our $VERSION = '0.01';
 1;
 
 __END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Alien::libdeflate - Fetch/build/stash the libdeflate headers and libs for
+L<libdeflate|https://github.com/ebiggers/libdeflate>
+
+=head1 SYNOPSIS
+
+In your C<Makefile.PL> with L<ExtUtils::MakeMaker>.
+
+  use Alien::libdeflate;
+  use ExtUtils::MakeMaker;
+  use Alien::Base::Wrapper qw( Alien::libdeflate !export );
+  use Config;
+
+  WriteMakefile(
+    # ...
+    Alien::Base::Wrapper->mm_args,
+    # ...
+    );
+
+In your script or module.
+
+  use Alien::libdeflate;
+  use Env qw( @PATH );
+
+  unshift @PATH, Alien::libdeflate->bin_dir;
+
+=head1 DESCRIPTION
+
+Download, build, and install the libdeflate C headers and libraries into a
+well-known location, C<<< Alien::libdeflate->dist_dir >>>, from whence other
+packages can make use of them.
+
+The version installed will be the latest release on the master branch from
+the libdeflate GitHub L<repository|https://github.com/ebiggers/libdeflate>.
+
+=head1 AUTHOR
+
+kiwiroy
+
+=cut
--- t/basic.t
+++ t/basic.t
@@ -7,4 +7,16 @@ use Alien::libdeflate;
 
 alien_ok 'Alien::libdeflate';
 
+run_ok(['gzip', '-h'])
+  ->success
+  ->out_like(qr/^usage:\s+gzip\s/mi);
+
+run_ok(['gunzip', '-h'])
+  ->success
+  ->out_like(qr/^usage:\s+gunzip\s/mi);
+
+run_ok(['checksum', '-h'])
+  ->success
+  ->out_like(qr/^usage:\s+checksum\s/mi);
+
 done_testing;
